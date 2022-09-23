ea72dc49164e5ed3c00bf7ff3d6e6874075bd0b0
unknown <Komal>
2022-09-24 02:49:55 +0530

    [Komal] Refactor

---
+++ MoodAnalyzer/UC1Refactor.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoodAnalyzer
+{
+    public class UC1Refactor
+    {
+        public string message;
+
+        public UC1Refactor(string message)
+        {
+            this.message = message;
+        }
+
+        public string AnalyseMood()
+        {
+            message = message.ToLower();
+            if (message.Contains("happy"))
+            {
+                return "happy";
+            }
+            else
+            {
+                return "sad";
+            }
+        }
+    }
+}
