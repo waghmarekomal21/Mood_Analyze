d2bf2187d171e706f6e90c9d3743611bc9131ab5
unknown <Komal>
2022-09-24 02:51:19 +0530

    [Komal] Invalid Mood

---
+++ MoodAnalyzer/UC2InvalidMood.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoodAnalyzer
+{
+    public class UC2InvalidMood
+    {
+        public string message;
+
+        public UC2InvalidMood(string message)
+        {
+            this.message = message;
+        }
+
+        public string AnalyseMood()
+        {
+            try
+            {
+                message = message.ToLower();
+                if (message.Contains("happy"))
+                {
+                    return "happy";
+                }
+                else
+                {
+                    return "sad";
+                }
+            }
+            catch (Exception ex)
+            {
+                return "happy";
+            }
+        }
+    }
+}
