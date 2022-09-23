5da401a39836177f98e1d845a27a1feeb4e72a2f
unknown <Komal>
2022-09-24 02:49:08 +0530

    [Komal] Ability to analyze happy or sad mood

---
+++ MoodAnalyzer/UC1MoodAnalyse.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoodAnalyzer
+{
+    public class UC1MoodAnalyse
+    {
+        public string AnalyseMood(string message)
+        {
+            message = message.ToLower();
+            if (message.Contains("happy"))
+            {
+                return "happy";
+            }
+            else
+            {
+                return "sad";
+            }
+        }
+    }
+}
+Footer
