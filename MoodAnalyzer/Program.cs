852ff1eda38264287d9c31bcd3fa43c316913e3e
unknown <Komal>
2022-09-24 02:46:50 +0530

    [Komal] Program File

---
+++ MoodAnalyzer/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace MoodAnalyzer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Mood Analyser");
+        }
+    }
+}
